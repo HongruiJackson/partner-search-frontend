1e7fca4fb44341b236af92889e139dc88096ddc8
JacksonZHR <1024060689@qq.com>
2024-09-13 17:50:55 +0800

    feat: 用户类型修改

--- src/models/user.d.ts
+++ src/models/user.d.ts
@@ -6,41 +6,9 @@
  */
 export type UserType =  {
     /**
-     * 头像
-     */
-    avatarUrl?: string;
-    /**
-     * 创建时间
-     */
-    createTime?: string;
-    /**
-     * 邮箱
-     */
-    email?: string;
-    /**
-     * 性别，0 表示男性；1 表示女性
-     */
-    gender?: number;
-    /**
      * 主键
      */
     id?: number;
-    /**
-     * 逻辑是否删除
-     */
-    idDelete?: number;
-    /**
-     * 电话
-     */
-    phone?: string;
-    /**
-     * 用户具有的标签
-     */
-    tags?: string[];
-    /**
-     * 更新时间
-     */
-    updateTime?: string;
     /**
      * 登录账号
      */
@@ -50,16 +18,33 @@ export type UserType =  {
      */
     username?: string;
     /**
-     * 密码
+     * 用户具有的标签
      */
-    userPassword?: string;
+    tags?: string[];
     /**
-     * 用户角色 0 默认用户 1 管理员
+     * 头像
      */
-    userRole?: number;
+    avatarUrl?: string;
+    /**
+     * 性别，0 表示男性；1 表示女性
+     */
+    gender?: number;
+    /**
+     * 电话
+     */
+    phone?: string;
+    /**
+     * 邮箱
+     */
+    email?: string;
     /**
      * 账号状态，0 表示正常
      */
     userStatus?: number;
+    /**
+     * 用户角色 0 默认用户 1 管理员
+     */
+    userRole?: number;
+
 
 }
