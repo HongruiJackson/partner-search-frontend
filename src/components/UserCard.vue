4f0e633626c7d8d3d5a16f1e3f3a15c96f879d06
JacksonZHR <1024060689@qq.com>
2024-09-16 19:16:16 +0800

    feat: 用户展示卡片

---
+++ src/components/UserCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type {UserType} from "@/models/user";
+import type {PropType} from "vue";
+
+defineProps({
+  user: {
+    type: Object as PropType<UserType>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <van-card
+      desc="描述信息"
+      :title=user.userAccount
+      thumb="https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg"
+  >
+    <template #tags>
+      <van-tag plain type="primary" v-for="tag in user.tags">{{tag}}</van-tag>
+      <div>{{user}}</div>
+    </template>
+  </van-card>
+
+</template>
+
+<style scoped>
+
+</style>
