86c52040f77ad027e218ce76e702dccb2465867e
JacksonZHR <1024060689@qq.com>
2024-09-16 19:15:48 +0800

    mock: 测试数据

---
+++ src/mock/User.ts
@@ -0,0 +1,19 @@
+const User = {
+
+    id: 1,
+    userAccount: 'jackson',
+    tags: [
+        "Java",
+        "Python",
+        "JS"
+    ],
+    username: null,
+    avatarUrl: null,
+    gender: 0,
+    phone: null,
+    email: null,
+    userStatus: 0,
+    userRole: 1,
+}
+
+export default User
---
+++ src/mock/UserList.ts
@@ -0,0 +1,41 @@
+const UserList = [
+    {
+
+        id: 1,
+        userAccount: 'jackson',
+        tags: [
+            "Java",
+            "Python",
+            "JS"
+        ],
+        username: null,
+        avatarUrl: null,
+        gender: 0,
+        phone: null,
+        email: null,
+        userStatus: 0,
+        userRole: 1,
+    },
+    {
+
+        id: 3,
+        userAccount: 'ian',
+        tags: [
+            "Java",
+            "ES",
+            "JS",
+            "dwdw",
+            "iwjdoiwdpowpodewf",
+        ],
+        username: null,
+        avatarUrl: null,
+        gender: 0,
+        phone: null,
+        email: null,
+        userStatus: 0,
+        userRole: 1,
+
+    }
+]
+
+export default UserList
