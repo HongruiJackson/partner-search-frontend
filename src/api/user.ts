3fe0b0b470eb6bc9492659cc51bdddaa66d1954f
JacksonZHR <1024060689@qq.com>
2024-09-17 18:28:20 +0800

    feat: 修改一个常量名

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,9 +1,9 @@
 import request from '@/utils/request'
 import type {LocationQueryValue} from "vue-router";
 
-const url = '/user'
+const startUrl = '/user'
 /**
  * 根据标签查找用户
  */
 export const searchUserByTags = (tags: string | null | LocationQueryValue[]) =>
-    request.post(url+'/search/tags/all',tags)
+    request.post(startUrl+'/search/tags/all',tags)
