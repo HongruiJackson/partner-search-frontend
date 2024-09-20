8d98282565b88b211cee639cfa5bf24a91308eee
JacksonZHR <1024060689@qq.com>
2024-09-20 11:10:43 +0800

    chore: 删除冗余的手动添加cookie的操作

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -11,11 +11,6 @@ const instance = axios.create({
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
-    const userStore = useUserStore()
-    if (userStore.cookie) {
-        config.headers.Cookie = userStore.cookie
-    }
-
     return config;
 }, function (error) {
     // 对请求错误做些什么
