26bd337f5c35bde3c846a90618603d3f51c071e9
JacksonZHR <1024060689@qq.com>
2024-09-20 17:05:31 +0800

    feat: 主页基本代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import {onMounted, type Ref, ref} from "vue";
+import UserCard from "@/components/UserCard.vue";
+import mockList from "@/mock/UserList";
+
+let userList: Ref = ref([]);
+// const userList = mockUserList;
+onMounted(async () => {
+  // const res = await searchUserByTags(tags);
+  // console.log(res)
+  userList.value = mockList
+})
 
 </script>
 
 <template>
-  <div>Home</div>
+  <van-empty image="search" description="搜索结果为空" v-if="userList.length < 1" />
+  <div v-for="user in userList.values()" v-bind:key="user.userAccount" v-else>
+    <UserCard :user = user />
+  </div>
 </template>
 
 <style scoped>
