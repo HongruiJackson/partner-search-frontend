d7f84805cb80be026f88618be9b726a3dca8bb58
JacksonZHR <1024060689@qq.com>
2024-09-21 09:23:20 +0800

    feat: 新增未登录时的提示以及跳转登录页面按钮

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -28,25 +28,37 @@ onMounted(async ()=>{
   user.value = res.data.data
   userStore.setUser(user.value)
 })
+
+const toLogin = ()=> {
+  router.push({name: 'login'})
+}
 </script>
 
 <template>
-  <van-cell title="昵称" is-link :value="user.username==null?'未填写':user.username" @click="toEdit('username',user.username,'昵称')"/>
-  <van-cell title="账号" :value="user.userAccount==null?'未填写':user.userAccount"/>
-  <van-cell title="简介" is-link :value="user.profile==null?'未填写':user.profile" @click="toEdit('profile',user.profile,'简介')"/>
-<!--  <van-cell title="头像" is-link @click="toEdit('avatarUrl',user.avatarUrl,'头像')">-->
-  <van-cell title="头像">
-    <img width="46px" :src="user.avatarUrl==null?'src/assets/logo.svg':user.avatarUrl" alt="加载失败">
-  </van-cell>
-  <van-cell title="性别" is-link :value="user.gender?'女':'男'" @click="toEdit('gender',user.gender,'性别')"/>
-  <van-cell title="电话" is-link :value="user.phone==null?'未填写':user.phone" @click="toEdit('phone',user.phone,'电话')"/>
-  <van-cell title="邮箱" is-link :value="user.email==null?'未填写':user.email" @click="toEdit('email',user.email,'邮箱')"/>
+  <van-empty image="error" description="未登录" v-if="!user">
+    <van-button round type="primary" class="bottom-button" @click="toLogin">跳转登录</van-button>
+  </van-empty>
+  <div v-else>
+    <van-cell title="昵称" is-link :value="user.username==null?'未填写':user.username" @click="toEdit('username',user.username,'昵称')"/>
+    <van-cell title="账号" :value="user.userAccount==null?'未填写':user.userAccount"/>
+    <van-cell title="简介" is-link :value="user.profile==null?'未填写':user.profile" @click="toEdit('profile',user.profile,'简介')"/>
+    <!--  <van-cell title="头像" is-link @click="toEdit('avatarUrl',user.avatarUrl,'头像')">-->
+    <van-cell title="头像">
+      <img width="46px" :src="user.avatarUrl==null?'src/assets/logo.svg':user.avatarUrl" alt="加载失败">
+    </van-cell>
+    <van-cell title="性别" is-link :value="user.gender?'女':'男'" @click="toEdit('gender',user.gender,'性别')"/>
+    <van-cell title="电话" is-link :value="user.phone==null?'未填写':user.phone" @click="toEdit('phone',user.phone,'电话')"/>
+    <van-cell title="邮箱" is-link :value="user.email==null?'未填写':user.email" @click="toEdit('email',user.email,'邮箱')"/>
 
-  <div style="padding: 12px">
-    <van-button block type="primary" @click="tempPush">to login page temp</van-button>
+    <div style="padding: 12px">
+      <van-button block type="primary" @click="tempPush">to login page temp</van-button>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.bottom-button {
+  width: 160px;
+  height: 40px;
+}
 </style>
